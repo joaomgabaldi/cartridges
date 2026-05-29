c2a73f4fc5e395334415821d6d0bc0babbe165c3
João M. Gabaldi <167992334+joaomgabaldi@users.noreply.github.com>
2026-05-29 14:44:18 -0300

    Update steam_api_manager.py

--- cartridges/store/managers/steam_api_manager.py
+++ cartridges/store/managers/steam_api_manager.py
@@ -1,4 +1,3 @@
-# steam_api_manager.py
 #
 # Copyright 2023 Geoffrey Coulaud
 #
@@ -7,51 +6,60 @@
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+import re
+import requests
+from requests.exceptions import ConnectionError as RequestConnectionError
 from requests.exceptions import HTTPError, SSLError
-from urllib3.exceptions import ConnectionError as Urllib3ConnectionError
 
+from cartridges import shared
 from cartridges.game import Game
 from cartridges.store.managers.async_manager import AsyncManager
-from cartridges.utils.steam import (
-    SteamAPIHelper,
-    SteamGameNotFoundError,
-    SteamNotAGameError,
-    SteamRateLimiter,
-)
 
 
 class SteamAPIManager(AsyncManager):
-    """Manager in charge of completing a game's data from the Steam API"""
+    """Manager in charge of getting game information from Steam"""
 
-    retryable_on = (HTTPError, SSLError, Urllib3ConnectionError)
+    run_after = ()
+    retryable_on = (HTTPError, SSLError, RequestConnectionError)
 
-    steam_api_helper: SteamAPIHelper = None
-    steam_rate_limiter: SteamRateLimiter = None
-
-    def __init__(self) -> None:
-        super().__init__()
-        self.steam_rate_limiter = SteamRateLimiter()
-        self.steam_api_helper = SteamAPIHelper(self.steam_rate_limiter)
-
-    def main(self, game: Game, additional_data: dict) -> None:
-        # Skip non-Steam games
-        appid = additional_data.get("steam_appid", None)
-        if appid is None:
+    def main(self, game: Game, _additional_data: dict) -> None:
+        if not shared.schema.get_boolean("steam-metadata"):
             return
-        # Get online metadata
-        try:
-            online_data = self.steam_api_helper.get_api_data(appid=appid)
-        except (SteamNotAGameError, SteamGameNotFoundError):
-            game.update_values({"blacklisted": True})
-        else:
-            game.update_values(online_data)
+
+        if game.blacklisted:
+            return
+
+        app_id = None
+        if game.source == "steam":
+            app_id = game.game_id.split("_")[1]
+        elif hasattr(game, "steam_id") and game.steam_id:
+            app_id = game.steam_id
+
+        if app_id:
+            uri = f"https://store.steampowered.com/api/appdetails?appids={app_id}&l=en"
+            res = requests.get(uri, timeout=5)
+            res.raise_for_status()
+
+            json_data = res.json()
+            if not json_data or not json_data.get(str(app_id)) or not json_data[str(app_id)].get("success"):
+                return
+            
+            data = json_data[str(app_id)]["data"]
+
+            if not getattr(game, "developer", None) and data.get("developers"):
+                game.developer = data["developers"][0]
+                
+            if not getattr(game, "publisher", None) and data.get("publishers"):
+                game.publisher = data["publishers"][0]
+                
+            if not getattr(game, "release_year", None):
+                date_str = data.get("release_date", {}).get("date", "")
+                if date_str:
+                    match = re.search(r'\d{4}', date_str)
+                    if match:
+                        game.release_year = match.group()
+
+            game.save()
+            game.update()
