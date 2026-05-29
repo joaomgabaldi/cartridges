c2e62378870638d322bcf57b19a260c1ea24c01d
João M. Gabaldi <167992334+joaomgabaldi@users.noreply.github.com>
2026-05-28 21:37:11 -0300

    Update file_manager.py

--- cartridges/store/managers/file_manager.py
+++ cartridges/store/managers/file_manager.py
@@ -1,32 +1,12 @@
 # file_manager.py
-#
-# Copyright 2023 Geoffrey Coulaud
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-# SPDX-License-Identifier: GPL-3.0-or-later
-
-import json
-
 from cartridges import shared
 from cartridges.game import Game
 from cartridges.store.managers.async_manager import AsyncManager
 from cartridges.store.managers.steam_api_manager import SteamAPIManager
-
+from cartridges.utils.sqlite import get_conn
 
 class FileManager(AsyncManager):
-    """Manager in charge of saving a game to a file"""
+    """Manager in charge of saving a game to the database"""
 
     run_after = (SteamAPIManager,)
     signals = {"save-ready"}
@@ -35,25 +15,13 @@ class FileManager(AsyncManager):
         if additional_data.get("skip_save"):  # Skip saving when loading games from disk
             return
 
-        shared.games_dir.mkdir(parents=True, exist_ok=True)
-
-        attrs = (
-            "added",
-            "executable",
-            "game_id",
-            "source",
-            "hidden",
-            "last_played",
-            "name",
-            "developer",
-            "removed",
-            "blacklisted",
-            "version",
-        )
-
-        json.dump(
-            {attr: getattr(game, attr) for attr in attrs if attr},
-            (shared.games_dir / f"{game.game_id}.json").open("w", encoding="utf-8"),
-            indent=4,
-            sort_keys=True,
-        )
+        with get_conn() as conn:
+            conn.execute('''
+                INSERT OR REPLACE INTO games 
+                (game_id, added, executable, source, hidden, last_played, name, developer, removed, blacklisted, version)
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+            ''', (
+                game.game_id, game.added, game.executable, game.source,
+                game.hidden, game.last_played, game.name, game.developer,
+                game.removed, game.blacklisted, getattr(game, 'version', shared.SPEC_VERSION)
+            ))
